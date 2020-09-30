41a1615edd8c7e3f383a07b3bbd8cb6bda68f93c
André <AndreR@ppedv.de>
2020-09-30 14:57:42 +0200

    NUnit tests

---
+++ Calculator/Calculator.Tests_NUnit/CalcTest_Nunit.cs
@@ -0,0 +1,23 @@
+﻿using NUnit.Framework;
+
+namespace Calculator.Tests_NUnit
+{
+    [TestFixture]
+    public class CalcTest_Nunit
+    {
+        [Test]
+        public void Calc_Substracts_50_and_25_results_25()
+        {
+            //Arrange
+            var calc = new Calc();
+
+            //Act
+            var result = calc.Subtract(50, 25);
+
+            //Assert
+            Assert.AreEqual(25, result);
+            Assert.That(result, Is.EqualTo(25));
+
+        }
+    }
+}
