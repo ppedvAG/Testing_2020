6543f287faaffb5b17b45c0178cae2f3255798c2
André <AndreR@ppedv.de>
2020-09-30 16:29:02 +0200

    TDDBank: Tests wurden alle erstellt

--- TDDBank/TDDBank.Tests/BankAccountTests.cs
+++ TDDBank/TDDBank.Tests/BankAccountTests.cs
@@ -6,9 +6,87 @@ namespace TDDBank.Tests
     [TestClass]
     public class BankAccountTests
     {
+        ///Bankkonto
+        ///	- Kontostand abfragen
+        ///	- Betrag einzahlen(nicht Negativ) ✔
+        ///	- Betrag abheben(nicht Negativ) ✔
+        ///		- Darf nicht unter 0 fallen ✔
+        ///	- Neues Konto hat 0 als Kontostand ✔
+
         [TestMethod]
-        public void TestMethod1()
+        public void BankAccount_new_account_should_have_zero_as_balance()
         {
+            var ba = new BankAccount();
+            //🥰 👙 🍻🍻
+            Assert.AreEqual(0m, ba.Balance);
         }
+
+        [TestMethod]
+        public void BankAccount_can_deposit()
+        {
+            var ba = new BankAccount();
+
+            ba.Deposit(5m);
+            ba.Deposit(5m);
+
+            Assert.AreEqual(10m, ba.Balance);
+        }
+
+        [TestMethod]
+        public void BankAccount_deposit_a_negative_value_throwes()
+        {
+            var ba = new BankAccount();
+
+            Assert.ThrowsException<ArgumentException>(() => ba.Deposit(-5m));
+        }
+
+        [TestMethod]
+        public void BankAccount_deposit_a_zero_as_value_throws()
+        {
+            var ba = new BankAccount();
+
+            Assert.ThrowsException<ArgumentException>(() => ba.Deposit(0m));
+        }
+
+        [TestMethod]
+        public void BankAccount_can_withdraw()
+        {
+            var ba = new BankAccount();
+
+            ba.Deposit(10m);
+            ba.Withdraw(3m);
+            ba.Withdraw(2m);
+
+            Assert.AreEqual(5m, ba.Balance);
+        }
+
+        [TestMethod]
+        public void BankAccount_withdraw_a_negative_value_throwes()
+        {
+            var ba = new BankAccount();
+
+            Assert.ThrowsException<ArgumentException>(() => ba.Withdraw(-5m));
+        }
+
+        [TestMethod]
+        public void BankAccount_withdraw_a_zero_as_value_throws()
+        {
+            var ba = new BankAccount();
+
+            Assert.ThrowsException<ArgumentException>(() => ba.Withdraw(0m));
+        }
+
+        [TestMethod]
+        public void BankAccount_withdrawing_should_not_be_below_zero()
+        {
+            var ba = new BankAccount();
+
+            ba.Deposit(10m);
+            ba.Withdraw(3m);
+
+            Assert.ThrowsException<InvalidOperationException>(() => ba.Withdraw(8m));
+        }
+
+
     }
 }
--- TDDBank/TDDBank/BankAccount.cs
+++ TDDBank/TDDBank/BankAccount.cs
@@ -1,12 +1,19 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace TDDBank
 {
     public class BankAccount
     {
+        public decimal Balance { get; private set; }
+
+        public void Deposit(decimal v)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Withdraw(decimal v)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
