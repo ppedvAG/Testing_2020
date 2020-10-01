6f96456ffcba7c44069da5d7d545f746bae80888
André <AndreR@ppedv.de>
2020-10-01 11:27:12 +0200

    Entity Framework als Data Komponente hinzugefügt

---
+++ ppedv.MittagsHunger/ppedv.MittagsHunger.Data.EF/EfContext.cs
@@ -0,0 +1,14 @@
+﻿using ppedv.MittagsHunger.Model;
+using System.Data.Entity;
+
+namespace ppedv.MittagsHunger.Data.EF
+{
+    public class EfContext : DbContext
+    {
+        public DbSet<Bestelltag> Bestelltage { get; set; }
+        public DbSet<Bestellung> Bestellungen { get; set; }
+        public DbSet<Gericht> Gerichte { get; set; }
+        public DbSet<Lieferant> Lieferanten { get; set; }
+        public DbSet<Person> Personen { get; set; }
+    }
+}
