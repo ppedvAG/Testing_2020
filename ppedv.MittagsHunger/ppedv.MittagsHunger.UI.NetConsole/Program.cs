d31c67a58a03017f1bbb73f6b051c6c8282678f5
André <AndreR@ppedv.de>
2020-10-02 09:36:00 +0200

    DI mit AutoFac

--- ppedv.MittagsHunger/ppedv.MittagsHunger.UI.NetConsole/Program.cs
+++ ppedv.MittagsHunger/ppedv.MittagsHunger.UI.NetConsole/Program.cs
@@ -1,6 +1,9 @@
-﻿using ppedv.MittagsHunger.BusinessLogic;
+﻿using Autofac;
+using ppedv.MittagsHunger.BusinessLogic;
 using ppedv.MittagsHunger.Model;
+using ppedv.MittagsHunger.Model.Contracts;
 using System;
+using System.Reflection;
 using System.Text;
 
 namespace ppedv.MittagsHunger.UI.NetConsole
@@ -12,7 +15,17 @@ namespace ppedv.MittagsHunger.UI.NetConsole
             Console.WriteLine("*** MittagsHunger v0.1 GOLD EDITION ***");
             Console.OutputEncoding = Encoding.UTF8;
 
-            var core = new Core(new Data.EF.EfRepository()); //todo DI
+            var builder = new ContainerBuilder();
+
+            // Register individual components
+            //builder.RegisterInstance(new Data.EF.EfRepository()).As<IRepository>();
+            builder.RegisterAssemblyTypes(Assembly.LoadFrom(@"C:\Users\ar2\source\repos\ppedvAG\Testing_2020\ppedv.MittagsHunger\ppedv.MittagsHunger.Data.EF\bin\Debug\ppedv.MittagsHunger.Data.EF.dll"))
+                   .Where(t => t.Name.EndsWith("Repository"))
+                   .AsImplementedInterfaces();
+
+            var container = builder.Build();
+
+            var core = new Core(container.Resolve<IRepository>());
 
             foreach (var liefer in core.Repository.GetAll<Lieferant>())
             {
